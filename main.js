eaab5bc7080b58a694efa52db803cc021d31d81e
Alexandr <alexgriko29@gmail.com>
2022-06-28 11:36:20 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+
+const inp=document.querySelector('.todoinp');
+const inpbtn=document.querySelector('.todoinpbtn');
+const out=document.querySelector('.outblock');
+const del=document.querySelectorAll('.todobtn');
+const fitem=document.querySelector('.firstitem');
+const item=document.querySelectorAll('.todoitem');
+
+let task=[];
+
+inpbtn.addEventListener('click', ()=> {
+  if (inp.value==='') {
+    return;
+  } else {
+    task.push(inp.value);
+    inp.value='';
+    console.log(task);
+    console.log(item);
+    textout(task);
+    fitem.style.display='none';
+  }
+});
+
+const textout=(arr)=> {
+  let textarr='';
+  for (let i=0; i<arr.length; i++) {
+    textarr+=`<div class="todoitem" ondrop="drop(event)" ondragover="allowDrop(event)">
+    <div class="todotext">
+      <p> ${arr[i]}</p>
+    </div>
+    <div class="todopanel">
+      <button class="todobtn">Del</button>
+    </div>
+  </div>`
+
+  }
+  out.innerHTML=textarr;
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+
+
+
